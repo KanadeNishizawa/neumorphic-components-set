a3469ee01bca432486b75478e97360093be426a1
KanadeNishizawa <kanade27@icloud.com>
2020-12-02 22:11:16 +0900

    setup for scss

--- src/stories/assets/components/header/Header.vue
+++ src/stories/assets/components/header/Header.vue
@@ -8,7 +8,10 @@
           viewBox="0 0 32 32"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g fill="none" fill-rule="evenodd">
+          <g
+            fill="none"
+            fill-rule="evenodd"
+          >
             <path
               d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
               fill="#FFF"
@@ -27,23 +30,23 @@
       </div>
       <div>
         <my-button
-          size="small"
-          @onClick="onLogout"
-          label="Log out"
           v-if="user"
-        />
-        <my-button
           size="small"
-          @onClick="onLogin"
-          label="Log in"
-          v-if="!user"
+          label="Log out"
+          @onClick="onLogout"
         />
         <my-button
+          v-if="!user"
+          size="small"
+          label="Log in"
+          @onClick="onLogin"
+        />
+        <my-button
+          v-if="!user"
           primary
           size="small"
-          @onClick="onCreateAccount"
           label="Sign up"
-          v-if="!user"
+          @onClick="onCreateAccount"
         />
       </div>
     </div>
@@ -54,7 +57,7 @@
 import MyButton from "../button/Button.vue";
 
 export default {
-  name: "my-header",
+  name: "MyHeader",
 
   components: { MyButton },
 
