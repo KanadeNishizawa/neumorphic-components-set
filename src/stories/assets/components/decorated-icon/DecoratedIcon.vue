8bf43768c6591ff5726b87bf0f3c6aef40f9e9e0
KanadeNishizawa <kanade27@icloud.com>
2020-12-03 21:12:54 +0900

    add DecoratedIcon

---
+++ src/stories/assets/components/decorated-icon/DecoratedIcon.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="icon-button" class="container">
+    <span class="container__icon material-icons"> {{ icon }}</span>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: "schedule",
+      validator: function (value) {
+        return (
+          [
+            "person",
+            "schedule",
+            "room",
+            "category",
+            "search",
+            "get_app",
+            "insert_drive_file",
+            "style",
+          ].indexOf(value) !== -1
+        );
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../scss/main.scss";
+
+.container {
+  width: 4rem;
+  height: 4rem;
+  position: relative;
+  border-radius: 2em;
+  vertical-align: middle;
+  box-shadow: $liner-shadow-concave;
+  background: $primary-liner-light;
+  @include fixed;
+  &__icon {
+    text-shadow: $shadow-icon;
+    font-size: 2.6rem;
+    @include text-liner;
+    @include fixed;
+    @include center;
+  }
+}
+</style>
