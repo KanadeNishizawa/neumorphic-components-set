d4700bd5857da989782683699b6d49c79a806d9e
KanadeNishizawa <kanade27@icloud.com>
2020-12-11 14:17:01 +0900

    add Card

---
+++ src/stories/assets/components/card/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card"><ContentDummy /></div>
+</template>
+
+<script>
+import Vue from 'vue';
+import ContentDummy from './ContentDummy';
+
+export default {
+  components: {
+    ContentDummy
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../scss/main.scss';
+
+.card {
+  border: 1px solid $base;
+  width: 100%;
+  height: auto;
+  box-sizing: border-box;
+  background: $base-liner;
+  border-radius: $radius-3;
+  box-shadow: $shadow-base;
+  padding: $spacing-4 $spacing-6;
+}
+</style>
---
+++ src/stories/assets/components/card/ContentDummy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content-dummy">
+    <p>
+      This is a Card component.<br />
+      Insert contents here.
+    </p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {};
+</script>
+<style lang="scss" scoped>
+@import '../../scss/main.scss';
+
+.content-dummy {
+  color: $text-sub;
+  line-height: $multi-line;
+}
+</style>
